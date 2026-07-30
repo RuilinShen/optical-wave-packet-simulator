28bc7ff34816cacb2c656d770a69dafe66798637
沈瑞麟 <shenruilin2024@outlook.com>
2026-07-30 19:27:08 +0800

    更新了字体列表

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ sys.path.insert(0, ".")
 import streamlit as st
 import numpy as np
 import matplotlib
-matplotlib.rcParams["font.sans-serif"] = ["Microsoft YaHei", "SimHei"]
+matplotlib.rcParams["font.sans-serif"] = ["Microsoft YaHei", "SimHei", "WenQuanYi Micro Hei", "Noto Sans CJK SC"]
 matplotlib.rcParams["axes.unicode_minus"] = False
 import matplotlib.pyplot as plt
 # Also re-set after plt import
-plt.rcParams["font.sans-serif"] = ["Microsoft YaHei", "SimHei"]
+plt.rcParams["font.sans-serif"] = ["Microsoft YaHei", "SimHei", "WenQuanYi Micro Hei", "Noto Sans CJK SC"]
 plt.rcParams["axes.unicode_minus"] = False
 matplotlib.rcParams["font.sans-serif"] = ["Microsoft YaHei", "SimHei"]
 matplotlib.rcParams["axes.unicode_minus"] = False
